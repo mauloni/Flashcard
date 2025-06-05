8b564f44c1d15105d215ad9a3d11fdc84afbf3a9
mauloni <ariel.mauloni@escola.pr.gov.br>
2025-06-05 13:08:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,39 @@
+criaCartao(
+    'Programação',
+    '. Qual é o nome completo do Corinthians',
+ ' Sport Club Corinthians Paulista'
+)
+
+criaCartao(
+    'Programação',
+    'Quando o Corinthians foi fundado?',
+    'O Corinthians foi fundado em 1º de setembro de 1910.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é o estádio oficial do Corinthians?',
+    'O estádio oficial é a Neo Química Arena.'
+)
+
+criaCartao(
+    'Programação',
+    'Quantos títulos da Libertadores o Corinthians conquistou?',
+    'uma vez, em 2012.'
+)
+
+
+
+criaCartao(
+    'Programação',
+    'Quem é o maior artilheiro da história do clube?',
+    ' é Cláudio Christóvam de Pinho'
+)
+
+
+
+criaCartao(
+    'Programação',
+    'Quais são as principais cores do Corinthians?',
+    ' são preto e branco.'
+)
